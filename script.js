cff70a5691936be725d82e1be300a81f2d5fe654
EMAXthedestroyer <emmanuel.adefuye74@gmail.com>
2020-03-04 10:00:52 -0600

    COntact Form JavaScript Update

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 function easterEgg(){alert("You discovered an Easter Egg, nice!");}//Easter Egg
 
 //-------------------------C O N T A C T  F O R M--------------------------
@@ -9,16 +7,15 @@ function contactForm(form)
   console.log("Contact Form is Working");
 
   var firstName = document.getElementById('firstName').value;
-  var lastName = document.getElementById('lastName').value;
   var email = document.getElementById('email').value;
 
-  if(firstName, lastName, email == "")
+  if(firstName, email == "")
   {
     alert("You're Missing Some Information In The Contact Form");
   }
   else
   {
-  console.log(firstName +' '+lastName+' '+email);
+  console.log(firstName +' '+email);
   document.getElementById('sentMessage').innerHTML="Message Sent";
   resetForm();
   sendMail();
@@ -34,12 +31,3 @@ function sendMail(){}
 function createTextFile()
 {}
 
-function App()
-{
-  return
-  (
-    <Layout>
-      <p>Trying React JS</p>
-    </Layout>
-  );
-}
